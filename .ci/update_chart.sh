ca3fde129ef55030aa1a9fe05260bac43fefaefe
Dennis Fricke <dennis.fricke@ninjaneers.de>
2020-01-22 13:52:55 +0100

    fix(chart): allow to upload huge files

--- .ci/update_chart.sh
+++ .ci/update_chart.sh
@@ -6,11 +6,11 @@ echo "Upload chart $NAME"
 
 curl -X PUT -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" \
   "https://api.github.com/repos/$TEAM/$REPOSITORY/contents/$NAME-$VERSION.tgz" \
-  -d "{
+  --data @<(printf '%s' "{
         \"message\":\"Chart update\",
         \"committer\": {
           \"name\": \"Chart Release Bot\",
           \"email\": \"charts@ninjaneers.de\"
         },
-        \"content\":\"$CONTENT\"
-      }"
+        \"content\":\"$(base64 $NAME-$VERSION.tgz | tr -d \\n)\"
+      }")
